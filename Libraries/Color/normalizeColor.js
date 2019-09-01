facebook/react-native

commit b47fae8a8df6a4f2fdd461e9f3cf0ad5788c5bec

add normalize/processColorObject.js for ios and macos (#146)
* add normalize/processColorObject.js for ios and macos

* run prettier

* Fix NativeColorType reference in normalizeColorObject

* return null in normalizeColorObject for invalid objects

* import NativeOrDynamicColorType from normalizeColorObject

* remove strict-local

* add normalize/processColorObject.android.js

* normalize/processColorObject param

* missed one NativeOrDynamicColorType import

* NativeOrDynamicColorType as processColorObject's return type

* ignore macos files in android's flowconfig

* Move normalizeColor require

* Add/fix TODO(macOS ISS#2323203) comments

Libraries/Color/normalizeColor.js
@@ -11,23 +11,16 @@
 /* eslint no-bitwise: 0 */
 'use strict';
 
-const Platform = require('Platform'); // [TODO(macOS ISS#2323203)
-
-export type SemanticOrDynamicColorType = {
-  semantic?: string,
-  dynamic?: {
-    light: ?(string | number | SemanticOrDynamicColorType),
-    dark: ?(string | number | SemanticOrDynamicColorType),
-  },
-}; // ]TODO(macOS ISS#2323203)
+const normalizeColorObject = require('normalizeColorObject'); // TODO(macOS ISS#2323203)
+import type {NativeOrDynamicColorType} from 'normalizeColorObject'; // TODO(macOS ISS#2323203)
 
 function normalizeColor(
   color: ?(
     | string
     | number
-    | SemanticOrDynamicColorType
+    | NativeOrDynamicColorType
   ) /* TODO(macOS ISS#2323203) */,
-): ?(number | SemanticOrDynamicColorType) /* TODO(macOS ISS#2323203) */ {
+): ?(number | NativeOrDynamicColorType) /* TODO(macOS ISS#2323203) */ {
   const matchers = getMatchers();
   let match;
 
@@ -39,26 +32,14 @@ function normalizeColor(
   }
 
   // [TODO(macOS ISS#2323203)
-  if (
-    typeof color === 'object' &&
-    color !== null &&
-    (Platform.OS === 'macos' || Platform.OS === 'ios')
-  ) {
-    if ('semantic' in color) {
-      // a macos semantic color
+  if (typeof color === 'object' && color !== null) {
+    const normalizedColorObj = normalizeColorObject(color);
+
+    if (normalizedColorObj !== null) {
       return color;
-    } else if ('dynamic' in color && color.dynamic !== undefined) {
-      // a dynamic, appearance aware color
-      const dynamic = color.dynamic;
-      const dynamicColor: SemanticOrDynamicColorType = {
-        dynamic: {
-          light: normalizeColor(dynamic.light),
-          dark: normalizeColor(dynamic.dark),
-        },
-      };
-      return dynamicColor;
     }
   }
+
   if (typeof color !== 'string') {
     return null;
   } // ]TODO(macOS ISS#2323203)

Libraries/Color/normalizeColorObject.macos.js
@@ -1,0 +1,45 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow
+ */
+// [TODO(macOS ISS#2323203)
+'use strict';
+
+const normalizeColor = require('normalizeColor');
+
+export type NativeOrDynamicColorType = {
+  semantic?: string,
+  dynamic?: {
+    light: ?(string | number | NativeOrDynamicColorType),
+    dark: ?(string | number | NativeOrDynamicColorType),
+  },
+};
+
+function normalizeColorObject(
+  color: NativeOrDynamicColorType,
+): ?(number | NativeOrDynamicColorType) {
+  if ('semantic' in color) {
+    // a macos semantic color
+    return color;
+  } else if ('dynamic' in color && color.dynamic !== undefined) {
+    // a dynamic, appearance aware color
+    const dynamic = color.dynamic;
+    const dynamicColor: NativeOrDynamicColorType = {
+      dynamic: {
+        light: normalizeColor(dynamic.light),
+        dark: normalizeColor(dynamic.dark),
+      },
+    };
+    return dynamicColor;
+  }
+
+  return null;
+}
+
+module.exports = normalizeColorObject;
+// ]TODO(macOS ISS#2323203)

Libraries/StyleSheet/processColor.js
@@ -11,14 +11,14 @@
 'use strict';
 
 const Platform = require('Platform');
-
+const processColorObject = require('processColorObject'); // TODO(macOS ISS#2323203)
 const normalizeColor = require('normalizeColor');
-import type {SemanticOrDynamicColorType} from 'normalizeColor'; // ]TODO(macOS ISS#2323203)
+import type {NativeOrDynamicColorType} from 'normalizeColorObject'; // TODO(macOS ISS#2323203)
 
 /* eslint no-bitwise: 0 */
 function processColor(
-  color?: ?(string | number | SemanticOrDynamicColorType),
-): ?(number | SemanticOrDynamicColorType) /* TODO(macOS ISS#2323203) */ {
+  color?: ?(string | number | NativeOrDynamicColorType),
+): ?(number | NativeOrDynamicColorType) /* TODO(macOS ISS#2323203) */ {
   if (color === undefined || color === null) {
     return color;
   }
@@ -28,23 +28,14 @@ function processColor(
     return undefined;
   }
 
-  // [TODO(macOS ISS#2323203)
-  if (
-    typeof int32Color === 'object' &&
-    (Platform.OS === 'macos' || Platform.OS === 'ios')
-  ) {
-    if ('dynamic' in int32Color && int32Color.dynamic !== undefined) {
-      const dynamic = int32Color.dynamic;
-      const dynamicColor = {
-        dynamic: {
-          light: processColor(dynamic.light),
-          dark: processColor(dynamic.dark),
-        },
-      };
-      return dynamicColor;
+  if (typeof int32Color === 'object') {
+    const processedColorObj = processColorObject(int32Color);
+
+    if (processedColorObj !== null) {
+      return processedColorObj;
     }
-    return int32Color;
   }
+
   if (typeof int32Color !== 'number') {
     return null;
   } // ]TODO(macOS ISS#2323203)

.flowconfig.android
@@ -1,6 +1,7 @@
 [ignore]
 ; We fork some components by platform
 .*/*[.]ios.js
+.*/*[.]macos.js
 
 ; Ignore templates for 'react-native init'
 <PROJECT_ROOT>/template/.*

Libraries/Color/normalizeColorObject.android.js
@@ -1,0 +1,22 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow
+ */
+// [TODO(macOS ISS#2323203)
+'use strict';
+
+export type NativeOrDynamicColorType = {};
+
+function normalizeColorObject(
+  color: NativeOrDynamicColorType,
+): ?(number | NativeOrDynamicColorType) {
+  return null;
+}
+
+module.exports = normalizeColorObject;
+// ]TODO(macOS ISS#2323203)

Libraries/Color/normalizeColorObject.ios.js
@@ -1,0 +1,45 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow
+ */
+// [TODO(macOS ISS#2323203)
+'use strict';
+
+export type NativeOrDynamicColorType = {
+  semantic?: string,
+  dynamic?: {
+    light: ?(string | number | NativeOrDynamicColorType),
+    dark: ?(string | number | NativeOrDynamicColorType),
+  },
+};
+
+function normalizeColorObject(
+  color: NativeOrDynamicColorType,
+): ?(number | NativeOrDynamicColorType) {
+  if ('semantic' in color) {
+    // a macos semantic color
+    return color;
+  } else if ('dynamic' in color && color.dynamic !== undefined) {
+    const normalizeColor = require('normalizeColor');
+
+    // a dynamic, appearance aware color
+    const dynamic = color.dynamic;
+    const dynamicColor: NativeOrDynamicColorType = {
+      dynamic: {
+        light: normalizeColor(dynamic.light),
+        dark: normalizeColor(dynamic.dark),
+      },
+    };
+    return dynamicColor;
+  }
+
+  return null;
+}
+
+module.exports = normalizeColorObject;
+// ]TODO(macOS ISS#2323203)

Libraries/Components/ActivityIndicator/ActivityIndicator.js
@@ -19,7 +19,7 @@ const RCTActivityIndicatorViewNativeComponent = require('RCTActivityIndicatorVie
 
 import type {NativeComponent} from 'ReactNative';
 import type {ViewProps} from 'ViewPropTypes';
-import type {SemanticOrDynamicColorType} from 'normalizeColor'; // ]TODO(macOS ISS#2323203)
+import type {NativeOrDynamicColorType} from 'normalizeColorObject'; // TODO(macOS ISS#2323203)
 
 const RCTActivityIndicator =
   Platform.OS === 'android'
@@ -54,7 +54,7 @@ type Props = $ReadOnly<{|
    *
    * See http://facebook.github.io/react-native/docs/activityindicator.html#color
    */
-  color?: ?(string | SemanticOrDynamicColorType), // ]TODO(macOS ISS#2323203)
+  color?: ?(string | NativeOrDynamicColorType), // TODO(macOS ISS#2323203)
 
   /**
    * Size of the indicator (default is 'small').

Libraries/Components/ActivityIndicator/RCTActivityIndicatorViewNativeComponent.js
@@ -15,7 +15,7 @@ const requireNativeComponent = require('requireNativeComponent');
 import type {ViewProps} from 'ViewPropTypes';
 import type {ViewStyleProp} from 'StyleSheet';
 import type {NativeComponent} from 'ReactNative';
-import type {SemanticOrDynamicColorType} from 'normalizeColor'; // ]TODO(macOS ISS#2323203)
+import type {NativeOrDynamicColorType} from 'normalizeColorObject'; // TODO(macOS ISS#2323203)
 
 type NativeProps = $ReadOnly<{|
   ...ViewProps,
@@ -39,7 +39,7 @@ type NativeProps = $ReadOnly<{|
    *
    * See http://facebook.github.io/react-native/docs/activityindicator.html#color
    */
-  color?: ?(string | SemanticOrDynamicColorType),
+  color?: ?(string | NativeOrDynamicColorType), // TODO(macOS ISS#2323203)
 
   /**
    * Size of the indicator (default is 'small').

microsoft/react-native-macos

commit 80b2509342d6451430c477d997d4ae46ce8ef77e

Fix regression caused by PR https://github.com/microsoft/react-native/pull/146.

Libraries/Color/normalizeColor.js
@@ -11,7 +11,6 @@
 /* eslint no-bitwise: 0 */
 'use strict';
 
-const normalizeColorObject = require('normalizeColorObject'); // TODO(macOS ISS#2323203)
 import type {NativeOrDynamicColorType} from 'normalizeColorObject'; // TODO(macOS ISS#2323203)
 
 function normalizeColor(
@@ -33,6 +32,8 @@ function normalizeColor(
 
   // [TODO(macOS ISS#2323203)
   if (typeof color === 'object' && color !== null) {
+    const normalizeColorObject = require('normalizeColorObject'); // TODO(macOS ISS#2323203)
+
     const normalizedColorObj = normalizeColorObject(color);
 
     if (normalizedColorObj !== null) {

Libraries/Color/normalizeColorObject.macos.js
@@ -10,8 +10,6 @@
 // [TODO(macOS ISS#2323203)
 'use strict';
 
-const normalizeColor = require('normalizeColor');
-
 export type NativeOrDynamicColorType = {
   semantic?: string,
   dynamic?: {
@@ -27,6 +25,8 @@ function normalizeColorObject(
     // a macos semantic color
     return color;
   } else if ('dynamic' in color && color.dynamic !== undefined) {
+    const normalizeColor = require('normalizeColor');
+
     // a dynamic, appearance aware color
     const dynamic = color.dynamic;
     const dynamicColor: NativeOrDynamicColorType = {

Libraries/StyleSheet/processColor.js
@@ -11,7 +11,6 @@
 'use strict';
 
 const Platform = require('Platform');
-const processColorObject = require('processColorObject'); // TODO(macOS ISS#2323203)
 const normalizeColor = require('normalizeColor');
 import type {NativeOrDynamicColorType} from 'normalizeColorObject'; // TODO(macOS ISS#2323203)
 
@@ -29,6 +28,8 @@ function processColor(
   }
 
   if (typeof int32Color === 'object') {
+    const processColorObject = require('processColorObject'); // TODO(macOS ISS#2323203)
+
     const processedColorObj = processColorObject(int32Color);
 
     if (processedColorObj !== null) {