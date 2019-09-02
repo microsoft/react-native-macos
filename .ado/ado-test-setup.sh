6d962cd5c62cf325d3736677bf8627bb8ef67dce
Tom Underhill <tomun@microsoft.com>
2019-09-02 16:05:20 -0700

    Update yaml scripts to use the Mojave agent pools

--- .ado/ado-test-setup.sh
+++ .ado/ado-test-setup.sh
@@ -6,7 +6,12 @@ set -ex
 THIS_DIR=$PWD
 
 # Start the packager
-osascript -e "tell application \"Terminal\" to do script \"cd ${THIS_DIR}; export SERVERS_NO_WAIT=1; ./scripts/launchPackager.command --reactNativePath ${THIS_DIR}\""
+# AppleScript can't be invoked from Azure DevOps Mojave agents until the following ticket is resolved: https://dev.azure.com/mseng/AzureDevOps/_workitems/edit/1513729
+# osascript -e "tell application \"Terminal\" to do script \"cd ${THIS_DIR}; export SERVERS_NO_WAIT=1; ./scripts/launchPackager.command --reactNativePath ${THIS_DIR}\""
+COMMAND=$TMPDIR/vsto-test-setup.command
+echo "cd ${THIS_DIR}; export SERVERS_NO_WAIT=1; ./scripts/launchPackager.command --reactNativePath ${THIS_DIR}" > $COMMAND
+chmod +x $COMMAND
+open $COMMAND
 
 # Start the WebSocket test server
 osascript -e "tell application \"Terminal\" to do script \"cd ${THIS_DIR}; export SERVERS_NO_WAIT=1; ./IntegrationTests/launchWebSocketServer.command\""
