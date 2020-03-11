29dfd72513db19d8b3a1bdc62c1902b8b91cd33d
Anandraj Govindan <anandrag@microsoft.com>
2020-03-11 10:00:05 -0700

    Adding prettierignore and making some fixes to the patches

--- android-patches/patches-droid-office-grouped/UIEditText/Libraries/Components/TextInput/TextInputState.js
+++ android-patches/patches-droid-office-grouped/UIEditText/Libraries/Components/TextInput/TextInputState.js
@@ -2,16 +2,16 @@
 +++ "E:\\github\\ms-react-native-forpatch\\Libraries\\Components\\TextInput\\TextInputState.js"	2020-02-27 14:13:23.259748900 -0800
 @@ -36,7 +36,6 @@
   */
- function focusTextInput(textFieldID: ?number) {
-   if (currentlyFocusedID !== textFieldID && textFieldID !== null) {
+  function focusTextInput(textFieldID: ?number) {
+    if (currentlyFocusedID !== textFieldID && textFieldID !== null) {
 -    currentlyFocusedID = textFieldID;
-  if (
-    Platform.OS === 'ios' ||
-    Platform.OS === 'macos' /* TODO(macOS ISS#2323203) */
+    if (
+      Platform.OS === 'ios' ||
+      Platform.OS === 'macos' /* TODO(macOS ISS#2323203) */
 @@ -76,6 +78,29 @@
-     }
-   }
- }
+    }
+  }
+}
 +
 +/** [TODO(android ISS)
 + * @param {number} TextInputID id of the text field that has received focus
@@ -35,15 +35,15 @@
 +  }
 +} // ]TODO(android ISS)
 +
- function registerInput(textFieldID: number) {
-   inputs.add(textFieldID);
- }
+function registerInput(textFieldID: number) {
+  inputs.add(textFieldID);
+}
 @@ -90,6 +115,8 @@
- 
- module.exports = {
-   currentlyFocusedField,
+
+module.exports = {
+  currentlyFocusedField,
 +  setFocusedTextInput, // TODO(android ISS)
 +  clearFocusedTextInput, // TODO(android ISS)
-   focusTextInput,
-   blurTextInput,
-   registerInput,
+  focusTextInput,
+  blurTextInput,
+  registerInput,
