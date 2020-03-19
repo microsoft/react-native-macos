0f5205de75f5640b0494c3ea73f55f55a7fee1d4
Anandraj Govindan <anandrag@microsoft.com>
2020-03-18 17:47:42 -0700

    Fixing some formatting

--- android-patches/patches-droid-office-grouped/UIEditText/Libraries/Components/TextInput/TextInput.js
+++ android-patches/patches-droid-office-grouped/UIEditText/Libraries/Components/TextInput/TextInput.js
@@ -1,22 +1,6 @@
 --- "E:\\github\\ms-react-native-forpatch\\Libraries\\Components\\TextInput\\TextInput.js"	2020-03-01 18:40:54.730428400 -0800
 +++ "E:\\github\\ms-react-native-minus\\Libraries\\Components\\TextInput\\TextInput.js"	2020-03-01 18:45:49.735945800 -0800
-@@ -1146,6 +1146,7 @@
-         rejectResponderTermination={true}
-         accessible={props.accessible}
-         accessibilityLabel={props.accessibilityLabel}
-+        accessibilityHint={props.accessibilityHint} // TODO(OSS Candidate ISS#2710739)
-         accessibilityRole={props.accessibilityRole}
-         accessibilityStates={props.accessibilityStates}
-         nativeID={this.props.nativeID}
-@@ -1255,6 +1256,7 @@
-         onPress={this._onPress}
-         accessible={this.props.accessible}
-         accessibilityLabel={this.props.accessibilityLabel}
-+        accessibilityHint={this.props.accessibilityHint} // TODO(OSS Candidate ISS#2710739)
-         accessibilityRole={this.props.accessibilityRole}
-         accessibilityStates={this.props.accessibilityStates}
-         nativeID={this.props.nativeID}
-@@ -1265,6 +1267,15 @@
+@@ -1267,6 +1267,15 @@
    },
  
    _onFocus: function(event: FocusEvent) {
@@ -32,7 +16,7 @@
      if (this.props.onFocus) {
        this.props.onFocus(event);
      }
-@@ -1360,7 +1371,17 @@
+@@ -1362,7 +1371,17 @@
    _onBlur: function(event: BlurEvent) {
      // This is a hack to fix https://fburl.com/toehyir8
      // @todo(rsnara) Figure out why this is necessary.
