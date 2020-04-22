5f23bddea41f9f744490c1f09fb674973caa8269
Anandraj Govindan <anandrag@microsoft.com>
2020-04-22 13:59:47 -0700

    Test

--- .ado/setup_droid_deps.sh
+++ .ado/setup_droid_deps.sh
@@ -7,3 +7,7 @@ ln -s ReactAndroid/packages/boost.1.68.0.0/lib/native/include/boost $BUILD_DEPS_
 ln -s double-conversion/double-conversion $BUILD_DEPS_DIR/double-conversion
 ln -s Folly/ $BUILD_DEPS_DIR/Folly
 ln -s glog/ $BUILD_DEPS_DIR/glog
+
+ls -l
+ls -l $BUILD_DEPS_DIR
+ls -l $BUILD_DEPS_DIR/boost
