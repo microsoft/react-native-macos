c81a929b21637758197078e46c7ed5c3a82899eb
Anandraj Govindan <anandrag@microsoft.com>
2020-04-22 14:58:53 -0700

    Test

--- .ado/setup_droid_deps.sh
+++ .ado/setup_droid_deps.sh
@@ -3,12 +3,13 @@ BUILD_DEPS_DIR=build_deps
 rm -rf $BUILD_DEPS_DIR
 mkdir $BUILD_DEPS_DIR
 
-ln -s ReactAndroid/packages/boost.1.68.0.0/lib/native/include/boost $BUILD_DEPS_DIR/boost
-ln -s double-conversion/double-conversion $BUILD_DEPS_DIR/double-conversion
-ln -s Folly/ $BUILD_DEPS_DIR/Folly
-ln -s glog/ $BUILD_DEPS_DIR/glog
+ln -s $PWD/ReactAndroid/packages/boost.1.68.0.0/lib/native/include/boost $BUILD_DEPS_DIR/boost
+ln -s $PWD/double-conversion/double-conversion $BUILD_DEPS_DIR/double-conversion
+ln -s $PWD/Folly/ $BUILD_DEPS_DIR/Folly
+ln -s $PWD/glog/ $BUILD_DEPS_DIR/glog
 
 ls -l
 ls -l $BUILD_DEPS_DIR
 ls -l $BUILD_DEPS_DIR/boost
 ls -l $BUILD_DEPS_DIR/boost/accumulators
+ls -l $BUILD_DEPS_DIR/Folly/folly
