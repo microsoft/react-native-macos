301e781cac30fb19220ba3a0c4b46a39c9704202
Devon Deonarine <hello@devondeonarine.ca>
2020-08-06 16:18:31 -0700

    iOS: Update RCTAlertManager to use new RCTAlertController (#29295)
    Summary:
    This should fix https://github.com/facebook/react-native/issues/29082 and https://github.com/facebook/react-native/issues/10471
    
    Currently when an alert is being shown while a modal is being dismissed, it causes the alert not to show and In some cases it causes the UI to become unresponsive. I think this was caused by using RCTPresentedViewController to try and display the Alert the currently presented view. The View the Alert was going to be shown on is dismissed and the modal doesn't show. I implemented a new RCTAlertController to show the alert on top of the view, the modal being dismissed should now not interfere with the alert being shown.
    
    [iOS] [Fixed] - Fixed showing Alert while closing a Modal
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/29295
    
    Test Plan:
    To recreate the bug:
    
    1. npx react-native init Test --version 0.63.0-rc.1
    2. Paste the following code into App.js
    
    ```javascript
    /**
     * Sample React Native App
     * https://github.com/facebook/react-native
     *
     * format
     * flow strict-local
     */
    
    import React from 'react';
    import {
      SafeAreaView,
      StyleSheet,
      View,
      Text,
      StatusBar,
      Modal,
      Alert
    } from 'react-native';
    
    const App: () => React$Node = () => {
      const [visible, setVisible] = React.useState(false)
    
      const onShowModal = () => {
        setVisible(true)
      }
    
      onCloseBroken = () => {
        setVisible(false)
        Alert.alert('Alert', 'Alert won\'t show')
      }
    
      onCloseWorking = () => {
        setVisible(false)
        setTimeout(() => Alert.alert('Alert', 'Works fine'), 10)
      }
    
      return (
        <>
          <StatusBar barStyle="dark-content" />
          <SafeAreaView style={styles.container}>
            <Text onPress={onShowModal}>Show modal</Text>
          </SafeAreaView>
          <Modal animationType="fade" visible={visible} onRequestClose={onCloseWorking} >
            <View style={styles.container}>
              <Text onPress={onCloseBroken}>Close modal immediately</Text>
              <Text onPress={onCloseWorking}>Close modal with delay</Text>
            </View>
          </Modal>
        </>
      )
    }
    
    const styles = StyleSheet.create({
      container: {
        flex: 1,
        alignItems: 'center',
        justifyContent: 'space-around',
      },
    })
    
    export default App
    
    ```
    
    3. cd Test && npx react-native run-ios
    4. Show the modal and click the `Close modal immediately` button
    
    The first button doesn't show the alert, the second does because it gets rendered after the modal view is dismissed. After this commit, the alert always shows on top of every view properly. You can test by pointing the react native package to my branch by modifying the package json file like this
    
    ```
        "react-native": "https://github.com/devon94/react-native.git#fix-ios-modal"
    ```
    
    I was unable to reproduce the case where it causes the UI to be responsive in the test app but was able to reproduce it in our react native app at work. I can provide a video later if needed but the code is too complex to simplify into a test case.
    
    Reviewed By: sammy-SC
    
    Differential Revision: D22783371
    
    Pulled By: PeteTheHeat
    
    fbshipit-source-id: 3e359645c610074ea855ee5686c59bdb9d6b696b

---
+++ React/CoreModules/RCTAlertController.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <React/RCTUIKit.h> // TODO(macOS ISS#2323203)
+
+@interface RCTAlertController : UIViewController // : UIAlertController
+
+#if !TARGET_OS_OSX // [TODO(macOS ISS#2323203)
+- (void)show:(BOOL)animated completion:(void (^)(void))completion;
+#endif // ]TODO(macOS ISS#2323203)
+
+@end
