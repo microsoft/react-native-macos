43c973f0176e7b1ba496c492fd9aaef3580abf7f
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2020-08-21 17:21:21 -0700

    Fix build break

--- RNTester/NativeModuleExample/NativeScreenshotManager.js
+++ RNTester/NativeModuleExample/NativeScreenshotManager.js
@@ -14,7 +14,8 @@ import type {TurboModule} from '../../Libraries/TurboModule/RCTExport';
 import * as TurboModuleRegistry from '../../Libraries/TurboModule/TurboModuleRegistry';
 
 export interface Spec extends TurboModule {
-  takeScreenshot(id: string, options:{[key:string]:string | number | boolean}): Promise<string>;
+  getConstants:() => {||};
+  takeScreenshot(id: string, options:{}): Promise<string>;
 }
 
-export const NativeModule = (TurboModuleRegistry.get<Spec>('ScreenshotManager'): Spec);
+export const NativeModule = (TurboModuleRegistry.get<Spec>('ScreenshotManager'): ?Spec);
--- RNTester/js/examples/Snapshot/SnapshotExample.js
+++ RNTester/js/examples/Snapshot/SnapshotExample.js
@@ -39,9 +39,13 @@ class ScreenshotExample extends React.Component<{...}, $FlowFixMeState> {
 
   // TODO(macOS ISS#2323203): alert needs two string arguments, passing an error results in crashing
   takeScreenshot = () => {
-    ScreenshotManager.takeScreenshot('window', {format: 'jpeg', quality: 0.8}) // See UIManager.js for options
-      .then(uri => this.setState({uri}))
-      .catch(error => Alert.alert('ScreenshotManager.takeScreenshot', error.message));
+    if (ScreenshotManager !== undefined && ScreenshotManager !== null) {
+      ScreenshotManager.takeScreenshot('window', {format: 'jpeg', quality: 0.8}) // See UIManager.js for options
+        .then(uri => this.setState({uri}))
+        .catch(error => Alert.alert('ScreenshotManager.takeScreenshot', error.message));
+    } else {
+      Alert.alert('ScreenshotManager.takeScreenshot', 'The turbo module is not installed.');
+    }
   };
 }
 
