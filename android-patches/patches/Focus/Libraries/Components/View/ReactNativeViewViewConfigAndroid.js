25d7da6d67622cdcf6581fa545c4df24dd80eda4
Anandraj <mganandraj@outlook.com>
2020-09-22 12:22:02 -0700

    Adding "captured" event in the config

--- android-patches/patches/Focus/Libraries/Components/View/ReactNativeViewViewConfigAndroid.js
+++ android-patches/patches/Focus/Libraries/Components/View/ReactNativeViewViewConfigAndroid.js
@@ -1,12 +1,13 @@
 --- /home/hermes/code/react-native-macos-fresh/Libraries/Components/View/ReactNativeViewViewConfigAndroid.js	2020-09-21 21:41:24.322788533 -0700
-+++ /home/hermes/code/react-native-macos/Libraries/Components/View/ReactNativeViewViewConfigAndroid.js	2020-09-19 12:14:19.918744241 -0700
-@@ -19,6 +19,11 @@
++++ /home/hermes/code/react-native-macos/Libraries/Components/View/ReactNativeViewViewConfigAndroid.js	2020-09-22 11:58:57.799665163 -0700
+@@ -19,6 +19,12 @@
          captured: 'onSelectCapture',
        },
      },
 +    topOnFocusChange: {
 +      phasedRegistrationNames: {
-+        bubbled: 'onFocusChange'
++        bubbled: 'onFocusChange',
++        captured: 'onFocusChangeCapture'
 +      }
 +    }
    },
