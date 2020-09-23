2bd471a8dd4de4f2f2903fde347fc34ed05470a7
Anandraj <mganandraj@outlook.com>
2020-09-23 12:23:12 -0700

    Fixing the lint errors by adding trailing commas

--- android-patches/patches/Focus/Libraries/Components/View/ReactNativeViewViewConfigAndroid.js
+++ android-patches/patches/Focus/Libraries/Components/View/ReactNativeViewViewConfigAndroid.js
@@ -1,5 +1,5 @@
 --- /home/hermes/code/react-native-macos-fresh/Libraries/Components/View/ReactNativeViewViewConfigAndroid.js	2020-09-21 21:41:24.322788533 -0700
-+++ /home/hermes/code/react-native-macos/Libraries/Components/View/ReactNativeViewViewConfigAndroid.js	2020-09-22 11:58:57.799665163 -0700
++++ /home/hermes/code/react-native-macos/Libraries/Components/View/ReactNativeViewViewConfigAndroid.js	2020-09-23 12:20:05.571823280 -0700
 @@ -19,6 +19,12 @@
          captured: 'onSelectCapture',
        },
@@ -7,9 +7,9 @@
 +    topOnFocusChange: {
 +      phasedRegistrationNames: {
 +        bubbled: 'onFocusChange',
-+        captured: 'onFocusChangeCapture'
-+      }
-+    }
++        captured: 'onFocusChangeCapture',
++      },
++    },
    },
    directEventTypes: {
      topClick: {
