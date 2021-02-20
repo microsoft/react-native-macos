9b88cd16416eb825e0b561f9cf3035978ab754f8
Mo Wang <mowang@fb.com>
2021-02-20 11:21:47 -0800

    address PR comments

--- Libraries/Image/RCTUIImageViewAnimated.m
+++ Libraries/Image/RCTUIImageViewAnimated.m
@@ -90,11 +90,10 @@ static NSUInteger RCTDeviceFreeMemory() {
   }
 
 #if TARGET_OS_OSX
-  super.image = image;
+  [super setImage:image];
 #else // TODO(macOS ISS#2323203)
   [self stop];
   [self resetAnimatedImage];
-  
   if ([image respondsToSelector:@selector(animatedImageFrameAtIndex:)]) {
     NSUInteger animatedImageFrameCount = ((UIImage<RCTAnimatedImage> *)image).animatedImageFrameCount;
     // In case frame count is 0, there is no reason to continue.
