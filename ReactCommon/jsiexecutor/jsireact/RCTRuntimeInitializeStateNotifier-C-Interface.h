7ff61b89316f98bc927e0d562e26bd6b112fb661
Chris Hogan <chrishog@microsoft.com>
2021-04-14 22:58:51 -0700

    sequence runtime initialization with bundle load

---
+++ ReactCommon/jsiexecutor/jsireact/RCTRuntimeInitializeStateNotifier-C-Interface.h
@@ -0,0 +1,7 @@
+//
+//  RCTRuntimeInitializeStateNotifier-C-Interface.h
+//
+// TODO(OSS Candidate ISS#2710739)
+
+// This is the C function that will be used to invoke the Objctive-C notification posting method from C++
+void NotifyRuntimeInitializationEnd(void);
