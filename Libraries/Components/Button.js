5b7625e8a90327e4acc7b90eca6e861dea1338fa
Saad Najmi <saadnajmi2@gmail.com>
2021-05-21 14:22:13 -0500

    Add support for modifying the key view loop (#769)
    * Update RCTCxxBridge.mm
    
    * Update RCTCxxBridge.mm
    
    * initial props
    
    * initial key loop standup
    
    * update props
    
    * podfile
    
    * Add a few refs
    
    * NextKeyView -> NextKeyViewTest
    
    * More stuff
    
    * commit local changes
    
    * rip
    
    * commit local WIP
    
    * WIP
    
    * Clean up code a but for debugging
    
    * Key view loop works on a second pass
    
    * Move code to componentDidMount, but I still need the console logs in onFocus...
    
    * Latest local changes
    
    * Taking Tomun's approach
    
    * Clean up code
    
    * More cleanup
    
    * more clean up
    
    * for chris
    
    * wip
    
    * wip
    
    * WIP to see the diff
    
    * Cleanup
    
    * More cleanup
    
    * I guess the onFocus hack is the way to go
    
    * Cleanup
    
    * Update tags
    
    * Fix flow checks
    
    * yarn lint check fix
    
    * comment out flipper
    
    * Uncomment Flipper. We should just fix the CI failure instead
    
    * Clean up example fix ifdefs
    
    * Remove prop from everyhing but View
    
    * More comments
    
    * Minor indent fix
    
    * Revert podifle changes
    
    * update podfile
    
    * lint fix
    
    Co-authored-by: Chris Hogan <chrishog@microsoft.com>
    Co-authored-by: HeyImChris <48299693+HeyImChris@users.noreply.github.com>

--- Libraries/Components/Button.js
+++ Libraries/Components/Button.js
@@ -154,6 +154,11 @@ type ButtonProps = $ReadOnly<{|
    * For arrow keys, add "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown",
    */
   validKeysUp?: ?Array<string>,
+
+  /*
+   * Specifies the Tooltip for the view
+   */
+  tooltip?: string,
   // ]TODO(OSS Candidate ISS#2710739)
 |}>;
 
@@ -208,9 +213,11 @@ class Button extends React.Component<ButtonProps> {
       onFocus, // TODO(OSS Candidate ISS#2710739)
       onBlur, // TODO(OSS Candidate ISS#2710739)
       onKeyDown,
+      onKeyUp,
       validKeysDown,
       validKeysUp,
       onKeyUp,
+      tooltip,
     } = this.props;
     const buttonStyles = [styles.button];
     const textStyles = [styles.text];
