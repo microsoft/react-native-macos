6409e6b2723dafdfa68af71bd22ec11716b09be0
Saad Najmi <sanajmi@microsoft.com>
2021-06-09 11:33:12 -0500

    fix indent

--- RNTester/RNTesterIntegrationTests/RCTLoggingTests.m
+++ RNTester/RNTesterIntegrationTests/RCTLoggingTests.m
@@ -62,7 +62,7 @@
 
 - (void)testLogging
 {
-// First console log call will fire after 2.0 sec, to allow for any initial log messages
+  // First console log call will fire after 2.0 sec, to allow for any initial log messages
   // that might come in (seeing this in tvOS)
   [_bridge enqueueJSCall:@"LoggingTestModule.logToConsoleAfterWait" args:@[@"Invoking console.log",@2000]];
   // Spin native layer for 1.9 sec
