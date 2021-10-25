b544204a78692ce624a81fe9e28b7ecc3fc5b910
Adam Gleitman <adam.gleitman@gmail.com>
2021-10-25 12:46:33 -0700

    Update Android patches

--- android-patches/patches/OfficeRNHost/ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java
+++ android-patches/patches/OfficeRNHost/ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java
@@ -1,5 +1,5 @@
---- "E:\\gh\\react-native-macos2\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\bridge\\ReactBridge.java"	2021-10-12 13:35:49.124099000 -0700
-+++ "E:\\gh\\react-native-macos\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\bridge\\ReactBridge.java"	2021-10-12 13:22:46.508704400 -0700
+--- ./ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java	2021-10-06 16:05:18.000000000 -0700
++++ /var/folders/vs/8_b205053dddbcv7btj0w0v80000gn/T/update-Ge4Sm3/merge/OfficeRNHost/ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java	2021-10-25 12:22:45.000000000 -0700
 @@ -31,6 +31,24 @@
      Systrace.beginSection(
          TRACE_TAG_REACT_JAVA_BRIDGE, "ReactBridge.staticInit::load:reactnativejni");
