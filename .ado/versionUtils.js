8877ce706484143e49a34122ce7d6f4387120043
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2021-10-28 15:06:22 -0700

    Update rntester lock file when bumping package version

--- .ado/versionUtils.js
+++ .ado/versionUtils.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 const path = require("path");
 const semver = require('semver');
+const {execSync} = require('child_process');
 
 const pkgJsonPath = path.resolve(__dirname, "../package.json");
 let publishBranchName = '';
@@ -41,8 +42,8 @@ function updateVersionsInFiles(patchVersionPrefix) {
     }
  
     pkgJson.version = releaseVersion;
-    fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
-    console.log(`Updating package.json to version ${releaseVersion}`);
+    console.log(`Bumping files to version ${releaseVersion}`);
+    execSync(`node ./scripts/bump-oss-version.js --rnmpublish ${releaseVersion}`, {stdio: 'inherit'});
   
     return {releaseVersion, branchVersionSuffix};
 }
