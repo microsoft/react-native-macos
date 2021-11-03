895811a0a432dd3f48a439f4af7e3999e84f0734
Chris Hogan <chrishog@microsoft.com>
2021-11-03 15:20:05 -0700

    add swift support for xcode13

---
+++ packages/rn-tester/RNTester-Bridging-Header.h
@@ -0,0 +1,4 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+//  This is a bridging header required on macOS to compile Swift with Xcode 13 TODO: macOS
