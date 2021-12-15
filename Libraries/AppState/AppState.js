e75b2224f9cab6f07e7f9877fa7f81ec1add6167
Chris Hogan <chrishog@microsoft.com>
2021-12-14 22:29:58 -0800

    add osx support

--- Libraries/AppState/AppState.js
+++ Libraries/AppState/AppState.js
@@ -50,7 +50,7 @@ class AppState {
       const emitter: NativeEventEmitter<NativeAppStateEventDefinitions> = new NativeEventEmitter(
         // T88715063: NativeEventEmitter only used this parameter on iOS. Now it uses it on all platforms, so this code was modified automatically to preserve its behavior
         // If you want to use the native module on other platforms, please remove this condition and test its behavior
-        Platform.OS !== 'ios' ? null : NativeAppState,
+        Platform.OS !== 'ios' && Platform.OS !== 'macos' ? null : NativeAppState,
       );
       this._emitter = emitter;
 
