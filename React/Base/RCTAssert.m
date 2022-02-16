92a88b8f15c4fd2c90822fc6363420d252dedb5c
Paige Sun <paigesun@fb.com>
2022-02-16 14:16:17 -0800

    Don't error to Logview APIs not supported by new architecture that are expected to happen often
    Summary:
    Changelog: [Internal]
    
    In the new architecture, when an interop component is being called, log instead of warn/error, since at the moment we expect this to happen often.
    
    Reviewed By: fkgozali
    
    Differential Revision: D34252666
    
    fbshipit-source-id: 971156a1cd9ef9b788f677c49fa2c55bd86ad4fa

--- React/Base/RCTAssert.m
+++ React/Base/RCTAssert.m
@@ -322,3 +322,12 @@ void RCTLogNewArchitectureValidation(RCTNotAllowedValidation type, id context, N
 
   RCTLogInfo(@"%@", validationMessage(type, context, extra));
 }
+
+void RCTLogNotAllowedForNewArchitecture(id context, NSString *extra)
+{
+  if (!newArchitectureViolationReporting) {
+    return;
+  }
+
+  RCTLogInfo(@"%@", getNewArchitectureViolationMessage(context, extra));
+}
