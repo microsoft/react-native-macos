00d2a1ff5e44784874075265fe0f26689563f3e3
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2022-05-05 00:14:05 +0400

    macos: Add deeplink code to default template

--- local-cli/generator-macos/templates/macos/HelloWorld-macOS/AppDelegate.m
+++ local-cli/generator-macos/templates/macos/HelloWorld-macOS/AppDelegate.m
@@ -2,6 +2,7 @@
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
+#import <React/RCTLinkingManager.h>
 
 @interface AppDelegate () <RCTBridgeDelegate>
 
@@ -15,8 +16,21 @@
   _bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:nil];
 }
 
+- (void)application:(NSApplication *)application openURLs:(NSArray<NSURL *> *)urls {
+  [RCTLinkingManager setInitialUrl:urls[0]];
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  [[NSAppleEventManager sharedAppleEventManager]
+   setEventHandler:self
+   andSelector:@selector(getURL:withReplyEvent:)
+   forEventClass:kInternetEventClass andEventID:kAEGetURL
+  ];
   // Insert code here to initialize your application
+}
+
+- (void)getURL:(NSAppleEventDescriptor *)event withReplyEvent:(NSAppleEventDescriptor *)reply {
+  [RCTLinkingManager getUrlEventHandler:event withReplyEvent:reply];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
