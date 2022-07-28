microsoft/react-native-macos

commit d6f39a54d44de78e4074dbe2f882035e3248b478

Turn on focus ring for multiline textinput (#1276)

Libraries/Text/TextInput/Multiline/RCTMultilineTextInputView.m
@@ -48,6 +48,8 @@
 #else // [TODO(macOS GH#774)
     _scrollView.documentView = _backedTextInputView;
     _scrollView.contentView.postsBoundsChangedNotifications = YES;
+    // Enable focus ring by default
+    _scrollView.enableFocusRing = YES;
     [self addSubview:_scrollView];
     
     // a register for those notifications on the content view.
@@ -89,6 +91,14 @@
   _scrollView.frame = UIEdgeInsetsInsetRect(self.bounds, reactBorderInsets);
   [self setNeedsLayout];
 }
+
+- (void)setEnableFocusRing:(BOOL)enableFocusRing {
+  [super setEnableFocusRing:enableFocusRing];
+  if ([_scrollView respondsToSelector:@selector(setEnableFocusRing:)]) {
+    [_scrollView setEnableFocusRing:YES];
+  }
+}
+
 #endif // ]TODO(macOS GH#774)
 
 #pragma mark - UIScrollViewDelegate

React/Base/RCTUIKit.h
@@ -425,6 +425,8 @@ CGPathRef UIBezierPathCreateCGPathRef(UIBezierPath *path);
 @property (nonatomic, assign) CGFloat zoomScale;
 @property (nonatomic, assign) BOOL alwaysBounceHorizontal;
 @property (nonatomic, assign) BOOL alwaysBounceVertical;
+// macOS specific properties
+@property (nonatomic, assign) BOOL enableFocusRing;
 
 @end
 

React/Base/macOS/RCTUIKit.m
@@ -429,6 +429,19 @@ static RCTUIView *RCTUIViewCommonInit(RCTUIView *self)
 
 @implementation RCTUIScrollView // TODO(macOS ISS#3536887)
 
+- (void)setEnableFocusRing:(BOOL)enableFocusRing {
+  if (_enableFocusRing != enableFocusRing) {
+    _enableFocusRing = enableFocusRing;
+  }
+
+  if (enableFocusRing) {
+    // NSTextView has no focus ring by default so let's use the standard Aqua focus ring.
+    [self setFocusRingType:NSFocusRingTypeExterior];
+  } else {
+    [self setFocusRingType:NSFocusRingTypeNone];
+  }
+}
+
 // UIScrollView properties missing from NSScrollView
 - (CGPoint)contentOffset
 {

commit 0e50a38f0d87529fc5300171e52e7eaebd60d1c7

Fix enableFocusRing also enabled for multiline textFields
This fixes a regression introduced in https://github.com/microsoft/react-native-macos/pull/1276
We should respect the boolean flag sent from the JS to enable/disable the focusRing

Libraries/Text/TextInput/Multiline/RCTMultilineTextInputView.m
@@ -95,7 +95,7 @@
 - (void)setEnableFocusRing:(BOOL)enableFocusRing {
   [super setEnableFocusRing:enableFocusRing];
   if ([_scrollView respondsToSelector:@selector(setEnableFocusRing:)]) {
-    [_scrollView setEnableFocusRing:YES];
+    [_scrollView setEnableFocusRing:enableFocusRing];
   }
 }
 