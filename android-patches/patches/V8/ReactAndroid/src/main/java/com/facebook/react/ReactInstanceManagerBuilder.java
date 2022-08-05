2fa0e8eebc97a7937ac684b878eddc8fdc1a8465
Anandraj <anandrag@microsoft.com>
2022-08-05 15:36:10 +0530

    Some more fixes in the patches

--- android-patches/patches/V8/ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManagerBuilder.java
+++ android-patches/patches/V8/ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManagerBuilder.java
@@ -1,6 +1,15 @@
 --- "C:\\github\\react-native\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\ReactInstanceManagerBuilder.java"	2022-08-05 12:59:58.286964700 +0530
-+++ "C:\\github\\react-native-macos\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\ReactInstanceManagerBuilder.java"	2022-08-05 12:38:48.221666400 +0530
-@@ -397,6 +397,9 @@
++++ "C:\\github\\react-native-macos\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\ReactInstanceManagerBuilder.java"	2022-08-05 15:08:37.009898300 +0530
+@@ -34,6 +34,8 @@
+ import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+ import com.facebook.react.packagerconnection.RequestHandler;
+ import com.facebook.react.uimanager.UIImplementationProvider;
++import com.facebook.react.v8executor.V8ExecutorFactory;
++import com.facebook.react.v8executor.V8Executor;
+ import java.util.ArrayList;
+ import java.util.List;
+ import java.util.Map;
+@@ -397,6 +399,9 @@
      } else if (jsInterpreter == JSInterpreter.HERMES) {
        HermesExecutor.loadLibrary();
        return new HermesExecutorFactory();
