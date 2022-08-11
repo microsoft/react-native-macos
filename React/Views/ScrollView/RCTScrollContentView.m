77507aaf1de5c0c0dcad7f04a87b3e8a5373e8a5
chiuam <67026167+chiuam@users.noreply.github.com>
2022-08-11 16:26:04 -0400

    Fix accessibilityRole check

--- React/Views/ScrollView/RCTScrollContentView.m
+++ React/Views/ScrollView/RCTScrollContentView.m
@@ -55,7 +55,7 @@
     [[[scrollView bridge] uiManager] setLocalData:localData forView:self];
   }
 
-  if ([self accessibilityRole] == NSAccessibilityTableRole) {
+  if ([platformScrollView accessibilityRole] == NSAccessibilityTableRole) {
       NSMutableArray *subViews = [[NSMutableArray alloc] initWithCapacity:[[self subviews] count]];
       for (NSView *view in [self subviews]) {
           if ([view isKindOfClass:[RCTView class]]) {
