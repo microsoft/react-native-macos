c9c2d0f33bdeba5e051d7503d04ea3a06045fc08
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-08-31 17:41:51 -0700

    Add comment linking to github issue

--- packages/rn-tester/js/examples/AnimatedGratuitousApp/AnExSet.js
+++ packages/rn-tester/js/examples/AnimatedGratuitousApp/AnExSet.js
@@ -139,6 +139,7 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 25,
     color: 'white',
+    // TODO(macOS 1409)
     textShadowRadius: 3,
     textShadowColor: 'rgba(0, 0, 0, 1.0)',
     textShadowOffset: {height: 1, width: 0},
