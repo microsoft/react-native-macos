66dc0692ed74428880c05a412fb0b5da16a387c9
Navneet Kambo <nakambo@nakambo-mm.guest.corp.microsoft.com>
2022-09-02 09:07:49 -0700

    fix flow + lint

--- packages/rn-tester/js/examples/FocusOnMount/FocusOnMount.js
+++ packages/rn-tester/js/examples/FocusOnMount/FocusOnMount.js
@@ -14,35 +14,41 @@ const React = require('react');
 const ReactNative = require('react-native');
 const {Button, findNodeHandle, UIManager} = ReactNative;
 
-class FocusOnMountExample extends React.Component {
-    ref = React.createRef();
+class FocusOnMountExample extends React.Component<{}> {
+  ref = React.createRef();
 
-    componentDidMount() {
-        if (this.ref.current) {
-            const commands = UIManager.getViewManagerConfig('RCTView').Commands;
-            if ('focus' in commands) {
-              UIManager.dispatchViewManagerCommand(
-                findNodeHandle(this.ref.current),
-                UIManager.getViewManagerConfig('RCTView').Commands.focus,
-                undefined
-              );
-            }
-          }
+  componentDidMount() {
+    if (this.ref.current) {
+      const commands = UIManager.getViewManagerConfig('RCTView').Commands;
+      if ('focus' in commands) {
+        UIManager.dispatchViewManagerCommand(
+          findNodeHandle(this.ref.current),
+          UIManager.getViewManagerConfig('RCTView').Commands.focus,
+          undefined,
+        );
+      }
     }
+  }
 
-    render() {
-        return <Button ref={this.ref} title="This button will be focsued on mount" />;
-    }
-};
- 
+  render() {
+    return (
+      <Button
+        ref={this.ref}
+        title="This button will be focsued on mount"
+        onPress={() => {}}
+      />
+    );
+  }
+}
+
 exports.title = 'Focus On Mount';
 exports.description = 'Example for focusing a component on mount';
 exports.examples = [
-    {
-        title: 'FocusOnMountExample',
-        render: function (): React.Element<any> {
-            return <FocusOnMountExample />;
-        },
+  {
+    title: 'FocusOnMountExample',
+    render: function (): React.Element<any> {
+      return <FocusOnMountExample />;
     },
+  },
 ];
 // TODO(GH #1412)]
