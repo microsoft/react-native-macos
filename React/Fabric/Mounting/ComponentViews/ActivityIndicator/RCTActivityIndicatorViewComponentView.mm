8e63d8a3ec5e567ed974c6b7cb2becfb36f35a7c
Shawn Dempsey <shawndempsey@fb.com>
2022-11-30 13:51:39 -0800

    Add macOS tags

--- React/Fabric/Mounting/ComponentViews/ActivityIndicator/RCTActivityIndicatorViewComponentView.mm
+++ React/Fabric/Mounting/ComponentViews/ActivityIndicator/RCTActivityIndicatorViewComponentView.mm
@@ -8,7 +8,7 @@
 #import "RCTActivityIndicatorViewComponentView.h"
 
 #import <React/RCTConversions.h>
-#import <React/RCTActivityIndicatorView.h>
+#import <React/RCTActivityIndicatorView.h> // TODO(macOS GH#774)
 
 #import <react/renderer/components/rncore/ComponentDescriptors.h>
 #import <react/renderer/components/rncore/EventEmitters.h>
@@ -29,7 +29,7 @@ static UIActivityIndicatorViewStyle convertActivityIndicatorViewStyle(const Acti
 }
 
 @implementation RCTActivityIndicatorViewComponentView {
-  RCTActivityIndicatorView *_activityIndicatorView;
+  RCTActivityIndicatorView *_activityIndicatorView; // TODO(macOS GH#774)
 }
 
 #pragma mark - RCTComponentViewProtocol
@@ -45,7 +45,7 @@ static UIActivityIndicatorViewStyle convertActivityIndicatorViewStyle(const Acti
     static const auto defaultProps = std::make_shared<const ActivityIndicatorViewProps>();
     _props = defaultProps;
 
-    _activityIndicatorView = [[RCTActivityIndicatorView alloc] initWithFrame:self.bounds];
+    _activityIndicatorView = [[RCTActivityIndicatorView alloc] initWithFrame:self.bounds]; // TODO(macOS GH#774)
     _activityIndicatorView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 
     if (defaultProps->animating) {
