a632c8d452f86142307878612063cd233bcdae87
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-01-05 07:04:55 -0800

    feat: refactor `RCTKeyWindow` to be more resilient and work in multi-window apps (#42036)
    Summary:
    This PRs refactors `RCTKeyWindow()` to be more resilient and work in multi-window apps. After my recent PR got merged (https://github.com/facebook/react-native/issues/41935) it significantly reduced the number of calls to `RCTKeyWindow()` and now it's called only when necessary. So this PR makes this function a bit more resource intensive but it guarantees that we will find current scene's key window.
    
    This would also fix some brownfield scenarios where React Native is working in multi-window mode and in the future allow us to more easily adopt `UIWindowSceneDelegate`
    
    bypass-github-export-checks
    
    [IOS] [CHANGED] - refactor `RCTKeyWindow` to be more resilient and work in multi-window apps
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/42036
    
    Test Plan:
    Checkout RNTester example for Alerts and LoadingView.
    
    https://github.com/facebook/react-native/assets/52801365/8cf4d698-db6d-4a12-8d8d-7a5acf34858b
    
    Reviewed By: huntie
    
    Differential Revision: D52431720
    
    Pulled By: cipolleschi
    
    fbshipit-source-id: 0d6ef1d46b2428c30c9f64dae66b95dbc69f0a3b

--- packages/react-native/React/CoreModules/RCTDevLoadingView.mm
+++ packages/react-native/React/CoreModules/RCTDevLoadingView.mm
@@ -139,12 +139,14 @@ RCT_EXPORT_MODULE()
 
   dispatch_async(dispatch_get_main_queue(), ^{
     self->_showDate = [NSDate date];
+
     if (!self->_window && !RCTRunningInTestEnvironment()) {
 #if !TARGET_OS_OSX // [macOS]
-      UIWindow *window = RCTSharedApplication().keyWindow;
+      UIWindow *window = RCTKeyWindow();
       CGFloat windowWidth = window.bounds.size.width;
 
-      self->_window = [[UIWindow alloc] initWithFrame:CGRectMake(0, 0, windowWidth, window.safeAreaInsets.top + 10)];
+      self->_window = [[UIWindow alloc] initWithWindowScene:window.windowScene];
+      self->_window.frame = CGRectMake(0, 0, windowWidth, window.safeAreaInsets.top + 10);
       self->_label = [[UILabel alloc] initWithFrame:CGRectMake(0, window.safeAreaInsets.top - 10, windowWidth, 20)];
       [self->_window addSubview:self->_label];
 
@@ -184,6 +186,7 @@ RCT_EXPORT_MODULE()
 #else // [macOS
     self->_label.stringValue = message;
     self->_label.textColor = color;
+
     self->_label.backgroundColor = backgroundColor;
     [self->_window orderFront:nil];
 #endif // macOS]
