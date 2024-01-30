6fae9482ed57023258f8ccc1933b1216c938b0c2
Nick Lefever <lefever@meta.com>
2024-01-30 18:44:10 +0100

    [fabric] Add keyboard event handling to TextInput component
    Summary: This diff adds keyboard event handling to the TextInput component by testing the valid keys for manual keyboard event handling and dispatching key events to the View component keyboard handling method.
    
    Test Plan:
    - Run Workplace Chat with Fabric enabled.
    - Open a message thread with 3 or more participants.
    - Type `@` to display the reference selector.
    - Change the selection with the up and down arrows and select an entry with tab to test the custom keyboard event handling.
    
     https://pxl.cl/4ghtH
    
    Reviewers: shawndempsey, #rn-desktop
    
    Reviewed By: shawndempsey
    
    Differential Revision: https://phabricator.intern.facebook.com/D53241729
    
    Tasks: T157889406, T154618477

--- packages/react-native/React/Fabric/Mounting/ComponentViews/TextInput/RCTTextInputComponentView.mm
+++ packages/react-native/React/Fabric/Mounting/ComponentViews/TextInput/RCTTextInputComponentView.mm
@@ -478,7 +478,25 @@ using namespace facebook::react;
 
 
 - (BOOL)hasValidKeyDownOrValidKeyUp:(nonnull NSString *)key {
-  return YES;
+  std::string keyString = key.UTF8String;
+
+  if (_props->validKeysDown.has_value()) {
+    for (auto const &validKey : *_props->validKeysDown) {
+      if (validKey.key == keyString) {
+        return YES;
+      }
+    }
+  }
+
+  if (_props->validKeysUp.has_value()) {
+    for (auto const &validKey : *_props->validKeysUp) {
+      if (validKey.key == keyString) {
+        return YES;
+      }
+    }
+  }
+  
+  return NO;
 }
 
 - (void)submitOnKeyDownIfNeeded:(nonnull NSEvent *)event
@@ -565,7 +583,7 @@ using namespace facebook::react;
 }
 
 - (BOOL)textInputShouldHandleKeyEvent:(nonnull NSEvent *)event {
-  return YES;
+  return ![self handleKeyboardEvent:event];
 }
 
 - (BOOL)textInputShouldHandlePaste:(nonnull id<RCTBackedTextInputViewProtocol>)sender {
