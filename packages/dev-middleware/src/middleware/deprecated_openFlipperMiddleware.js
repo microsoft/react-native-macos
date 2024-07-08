de5a90c97c6d8126c7d8d3a00f485435810fa3f7
Saad Najmi <sanajmi@microsoft.com>
2024-07-08 16:16:57 -0700

    Copy over `dev-middleware` and `debugger-frontend` from RN Core main

--- packages/dev-middleware/src/middleware/deprecated_openFlipperMiddleware.js
+++ packages/dev-middleware/src/middleware/deprecated_openFlipperMiddleware.js
@@ -9,9 +9,9 @@
  * @oncall react_native
  */
 
+import type {Logger} from '../types/Logger';
 import type {NextHandleFunction} from 'connect';
 import type {IncomingMessage, ServerResponse} from 'http';
-import type {Logger} from '../types/Logger';
 
 import open from 'open';
 
@@ -45,6 +45,14 @@ export default function deprecated_openFlipperMiddleware({
           'Attempting to debug JS in Flipper (deprecated). This requires ' +
             'Flipper to be installed on your system to handle the ' +
             "'flipper://' URL scheme.",
+        );
+        logger?.info(
+          'In React Native 0.74, Flipper is no longer included for new React ' +
+            'Native projects. The Flipper React Native plugin is also ' +
+            'unsupported. You can continue to use Flipper to debug ' +
+            "your app's JavaScript code, however we recommend switching to " +
+            'a modern alternative.\nSee ' +
+            'https://reactnative.dev/docs/debugging#opening-the-debugger.',
         );
         await open(FLIPPER_SELF_CONNECT_URL);
         res.end();
--- packages/dev-middleware/src/middleware/openDebuggerMiddleware.js
+++ packages/dev-middleware/src/middleware/openDebuggerMiddleware.js
@@ -9,18 +9,16 @@
  * @oncall react_native
  */
 
-import type {NextHandleFunction} from 'connect';
-import type {IncomingMessage, ServerResponse} from 'http';
 import type {InspectorProxyQueries} from '../inspector-proxy/InspectorProxy';
-import type {BrowserLauncher, LaunchedBrowser} from '../types/BrowserLauncher';
+import type {BrowserLauncher} from '../types/BrowserLauncher';
 import type {EventReporter} from '../types/EventReporter';
 import type {Experiments} from '../types/Experiments';
 import type {Logger} from '../types/Logger';
+import type {NextHandleFunction} from 'connect';
+import type {IncomingMessage, ServerResponse} from 'http';
 
-import url from 'url';
 import getDevToolsFrontendUrl from '../utils/getDevToolsFrontendUrl';
-
-const debuggerInstances = new Map<string, ?LaunchedBrowser>();
+import url from 'url';
 
 type Options = $ReadOnly<{
   serverBaseUrl: string,
@@ -57,30 +55,52 @@ export default function openDebuggerMiddleware({
       (experiments.enableOpenDebuggerRedirect && req.method === 'GET')
     ) {
       const {query} = url.parse(req.url, true);
-      const {appId} = query;
+      const {
+        appId,
+        device,
+        launchId,
+        target: targetId,
+      }: {
+        appId?: string,
+        device?: string,
+        launchId?: string,
+        target?: string,
+        ...
+      } = query;
 
       const targets = inspectorProxy.getPageDescriptions().filter(
         // Only use targets with better reloading support
         app =>
-          app.title === 'React Native Experimental (Improved Chrome Reloads)',
+          app.title === 'React Native Experimental (Improved Chrome Reloads)' ||
+          app.reactNative.capabilities?.nativePageReloads === true,
       );
+
       let target;
 
       const launchType: 'launch' | 'redirect' =
         req.method === 'POST' ? 'launch' : 'redirect';
 
-      if (typeof appId === 'string') {
+      if (
+        typeof targetId === 'string' ||
+        typeof appId === 'string' ||
+        typeof device === 'string'
+      ) {
         logger?.info(
           (launchType === 'launch' ? 'Launching' : 'Redirecting to') +
             ' JS debugger (experimental)...',
         );
-        target = targets.find(_target => _target.description === appId);
-      } else {
+        target = targets.find(
+          _target =>
+            (targetId == null || _target.id === targetId) &&
+            (appId == null || _target.description === appId) &&
+            (device == null || _target.reactNative.logicalDeviceId === device),
+        );
+      } else if (targets.length > 0) {
         logger?.info(
           (launchType === 'launch' ? 'Launching' : 'Redirecting to') +
-            ' JS debugger for first available target...',
+            ` JS debugger${targets.length === 1 ? '' : ' for most recently connected target'}...`,
         );
-        target = targets[0];
+        target = targets[targets.length - 1];
       }
 
       if (!target) {
@@ -98,17 +118,18 @@ export default function openDebuggerMiddleware({
         return;
       }
 
+      const useFuseboxEntryPoint =
+        target.reactNative.capabilities?.prefersFuseboxFrontend;
+
       try {
         switch (launchType) {
           case 'launch':
-            await debuggerInstances.get(appId)?.kill();
-            debuggerInstances.set(
-              appId,
-              await browserLauncher.launchDebuggerAppWindow(
-                getDevToolsFrontendUrl(
-                  target.webSocketDebuggerUrl,
-                  serverBaseUrl,
-                ),
+            await browserLauncher.launchDebuggerAppWindow(
+              getDevToolsFrontendUrl(
+                experiments,
+                target.webSocketDebuggerUrl,
+                serverBaseUrl,
+                {launchId, useFuseboxEntryPoint},
               ),
             );
             res.end();
@@ -116,9 +137,10 @@ export default function openDebuggerMiddleware({
           case 'redirect':
             res.writeHead(302, {
               Location: getDevToolsFrontendUrl(
+                experiments,
                 target.webSocketDebuggerUrl,
-                // Use a relative URL.
-                '',
+                serverBaseUrl,
+                {relative: true, launchId, useFuseboxEntryPoint},
               ),
             });
             res.end();
@@ -130,7 +152,10 @@ export default function openDebuggerMiddleware({
           type: 'launch_debugger_frontend',
           launchType,
           status: 'success',
-          appId,
+          appId: appId ?? null,
+          deviceId: device ?? null,
+          resolvedTargetDescription: target.description,
+          prefersFuseboxFrontend: useFuseboxEntryPoint ?? false,
         });
         return;
       } catch (e) {
@@ -144,6 +169,7 @@ export default function openDebuggerMiddleware({
           launchType,
           status: 'error',
           error: e,
+          prefersFuseboxFrontend: useFuseboxEntryPoint ?? false,
         });
         return;
       }
