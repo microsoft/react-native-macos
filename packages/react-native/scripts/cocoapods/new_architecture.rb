fde55457462c594ee64e462b25294f83b108bca4
Saad Najmi <sanajmi@microsoft.com>
2025-03-19 05:34:03 -0500

    Add macOS tags

--- packages/react-native/scripts/cocoapods/new_architecture.rb
+++ packages/react-native/scripts/cocoapods/new_architecture.rb
@@ -189,6 +189,6 @@ class NewArchitectureHelper
     end
 
     def self.new_arch_enabled
-        return ENV["RCT_NEW_ARCH_ENABLED"] == "1"
+        return ENV["RCT_NEW_ARCH_ENABLED"] == "1" # [macOS] Disable Fabric by default till macOS supports it
     end
 end
