d0edc815fff40a7a66d4ad9ac21650298bf5107b
Saad Najmi <sanajmi@microsoft.com>
2025-03-23 23:50:07 -0700

    chore: manually release 0.78.0

--- packages/react-native/React/Base/RCTVersion.m
+++ packages/react-native/React/Base/RCTVersion.m
@@ -23,7 +23,7 @@ NSDictionary* RCTGetReactNativeVersion(void)
     __rnVersion = @{
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(78),
-                  RCTVersionPatch: @(1),
+                  RCTVersionPatch: @(0),
                   RCTVersionPrerelease: [NSNull null],
                   };
   });
