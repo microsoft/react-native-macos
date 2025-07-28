26428965f1f25489a0e945e5fd1b99295d1f48ee
Saad Najmi <sanajmi@microsoft.com>
2025-07-28 09:34:30 -0700

    Copy most of yarn monorepo's config

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -9,6 +9,157 @@ const {defineConfig} = require('@yarnpkg/types');
  * @typedef {import('@yarnpkg/types').Yarn.Constraints.Dependency} Dependency
  */
 
+const IGNORE_CONSISTENT_DEPENDENCIES_FOR = new Set([
+  `.`,
+  `packages/docusaurus`,
+]);
+
+/**
+ * This rule will enforce that a workspace MUST depend on the same version of a dependency as the one used by the other workspaces
+ * We allow Docusaurus to have different dependencies for now; will be addressed later (when we remove Gatsby)
+ * @param {Context} context
+ */
+function enforceConsistentDependenciesAcrossTheProject({Yarn}) {
+  for (const dependency of Yarn.dependencies()) {
+    if (IGNORE_CONSISTENT_DEPENDENCIES_FOR.has(dependency.workspace.cwd))
+      continue;
+
+    if (dependency.type === `peerDependencies`)
+      continue;
+
+    for (const otherDependency of Yarn.dependencies({ident: dependency.ident})) {
+      if (IGNORE_CONSISTENT_DEPENDENCIES_FOR.has(otherDependency.workspace.cwd))
+        continue;
+
+      if (otherDependency.type === `peerDependencies`)
+        continue;
+
+      if ((dependency.type === `devDependencies` || otherDependency.type === `devDependencies`) && Yarn.workspace({ident: otherDependency.ident}))
+        continue;
+
+      dependency.update(otherDependency.range);
+    }
+  }
+}
+
+/**
+ * This rule will enforce that a workspace MUST depend on the same version of a dependency as the one used by the other workspaces
+ * We allow Docusaurus to have different dependencies for now; will be addressed later (when we remove Gatsby)
+ * @param {Context} context
+ */
+function enforceWorkspaceDependenciesWhenPossible({Yarn}) {
+  for (const dependency of Yarn.dependencies()) {
+    if (!Yarn.workspace({ident: dependency.ident}))
+      continue;
+
+    dependency.update(`workspace:*`);
+  }
+}
+
+/**
+ * @param {Context} context
+ * @param {string} ident
+ * @param {string} explanation
+ */
+function forbidDependency({Yarn}, ident, explanation) {
+  for (const dependency of Yarn.dependencies({ident})) {
+    dependency.error(explanation);
+  }
+}
+
+/**
+ * @param {Context} context
+ * @param {Record<string, ((workspace: Workspace) => any) | string>} fields
+ */
+function enforceFieldsOnAllWorkspaces({Yarn}, fields) {
+  for (const workspace of Yarn.workspaces()) {
+    for (const [field, value] of Object.entries(fields)) {
+      workspace.set(field, typeof value === `function` ? value(workspace) : value);
+    }
+  }
+}
+
+/**
+ * @param {Context} context
+ * @param {string} ident
+ * @param {string} otherIdent
+ * @param {boolean} mustExist
+ */
+function enforceDependencyRelationship({Yarn}, ident, otherIdent, mustExist) {
+  for (const dependency of Yarn.dependencies({ident})) {
+    if (dependency.type === `peerDependencies`)
+      continue;
+
+    const hasOtherDependency = Yarn.dependency({
+      workspace: dependency.workspace,
+      ident: otherIdent,
+    });
+
+    if (mustExist) {
+      if (hasOtherDependency)
+        continue;
+
+      dependency.error(`The presence of ${ident} in ${dependency.type} mandates the presence of ${otherIdent}`);
+    } else {
+      if (!hasOtherDependency)
+        continue;
+
+      dependency.error(`The presence of ${ident} in ${dependency.type} forbids the presence of ${otherIdent}`);
+    }
+  }
+}
+
+
+/**
+ * Validate that all peer dependencies are provided. If one isn't, the
+ * constraint will try to fix it by looking at what's used in the other
+ * workspaces of the project. If it doesn't find any way to satisfy the
+ * dependency, it will generate an error.
+ *
+ * @param {Context} context
+ */
+function enforcePeerDependencyPresence({Yarn}) {
+  for (const workspace of Yarn.workspaces()) {
+    // The Gatsby website is pretty much deprecated anyway
+    if (workspace.cwd === `packages/gatsby`)
+      continue;
+
+    for (const dependency of Yarn.dependencies({workspace})) {
+      if (dependency.type === `peerDependencies`)
+        continue;
+
+      if (!dependency.resolution)
+        continue;
+
+      for (const peerName of dependency.resolution.peerDependencies.keys()) {
+        // Webpack plugins have peer dependencies but don't often need it; weird
+        if (peerName === `webpack`)
+          continue;
+
+        if (dependency.resolution.dependencies.has(peerName))
+          continue;
+
+        const otherDeps = Yarn.dependencies({ident: peerName})
+          .filter(otherDep => otherDep.type !== `peerDependencies`);
+
+        if (otherDeps.length === 0)
+          workspace.error(`Missing dependency on ${peerName} (required by ${dependency.ident})`);
+
+        // If the workspace has itself a peer dependency of the same name, then
+        // we assume that it'll be fulfilled by its ancestors in the dependency
+        // tree, so we only need to add the dependency to devDependencies.
+        const autofixTarget = Yarn.dependency({workspace, ident: peerName, type: `peerDependencies`})
+          ? `devDependencies`
+          : `dependencies`;
+
+        for (const otherDep of otherDeps) {
+          workspace.set([autofixTarget, peerName], otherDep.range);
+        }
+      }
+    }
+  }
+}
+
 /**
  * Enforce that react-native-macos declares a peer dependency on react-native on release branches,
  * except on the main branch, where there is no published version of React Native to align to.
@@ -109,6 +260,24 @@ function enforceReactNativeMacosVersionConsistency({Yarn}) {
 
 module.exports = defineConfig({
   constraints: async ctx => {
+    // Constraints copied from the Yarn monorepo
+    enforceConsistentDependenciesAcrossTheProject(ctx);
+    enforceWorkspaceDependenciesWhenPossible(ctx);
+    enforcePeerDependencyPresence(ctx);
+    // enforceFieldsOnAllWorkspaces(ctx, {
+    //   license: `MIT`,
+    //   // When changing the engines.node value check https://node.green/ for
+    //   // which ECMAScript version is fully supported and update the following files as needed:
+    //   // - tsconfig.json
+    //   // - packages/eslint-config/index.js
+    //   // - packages/yarnpkg-builder/sources/commands/new/plugin.ts
+    //   [`engines.node`]: `>=18.12.0`,
+    //   [`repository.type`]: `git`,
+    //   [`repository.url`]: `git+https://github.com/yarnpkg/berry.git`,
+    //   [`repository.directory`]: workspace => workspace.cwd,
+    // });
+
+    // React Native macOS specific constraints
     expectReactNativePeerDependency(ctx);
     enforceReactNativeVersionConsistency(ctx);
     enforceReactNativeMacosVersionConsistency(ctx);
