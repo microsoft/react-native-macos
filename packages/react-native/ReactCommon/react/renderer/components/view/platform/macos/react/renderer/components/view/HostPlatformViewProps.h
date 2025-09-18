9719891f8f76090cbd499c3db545f720e9951480
Saad Najmi <sanajmi@microsoft.com>
2025-09-17 19:38:00 -0700

    feat(fabric): Add macOS host platform to ReactCommon (#2690)

---
+++ packages/react-native/ReactCommon/react/renderer/components/view/platform/macos/react/renderer/components/view/HostPlatformViewProps.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+ // [macOS]
+
+#pragma once
+
+#include <react/renderer/components/view/BaseViewProps.h>
+#include <react/renderer/components/view/primitives.h>
+#include <react/renderer/core/Props.h>
+#include <react/renderer/core/PropsParserContext.h>
+
+#include "MacOSViewEvents.h"
+
+namespace facebook::react {
+
+class HostPlatformViewProps : public BaseViewProps {
+ public:
+  HostPlatformViewProps() = default;
+  HostPlatformViewProps(
+      const PropsParserContext& context,
+      const HostPlatformViewProps& sourceProps,
+      const RawProps& rawProps);
+
+  void setProp(
+      const PropsParserContext& context,
+      RawPropsPropNameHash hash,
+      const char* propName,
+      const RawValue& value);
+
+  MacOSViewEvents macOSViewEvents{};
+
+#pragma mark - Props
+
+  bool focusable{false};
+  bool enableFocusRing{true};
+
+};
+} // namespace facebook::react
