6d33eb28aad450c3feade7fd041ed3f7813ffc94
Saad Najmi <sanajmi@microsoft.com>
2025-09-18 19:02:36 -0700

    Copy RNW's KeyEvent.h

--- packages/react-native/ReactCommon/react/renderer/components/view/platform/macos/react/renderer/components/view/KeyEvent.h
+++ packages/react-native/ReactCommon/react/renderer/components/view/platform/macos/react/renderer/components/view/KeyEvent.h
@@ -1,63 +1,29 @@
 /*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * Copyright (c) Microsoft Corporation.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
+ // [macOS]
+
 #pragma once
 
 #include <react/renderer/core/PropsParserContext.h>
 #include <react/renderer/core/propsConversions.h>
-#include <optional>
+#include <react/renderer/graphics/Float.h>
 #include <string>
 
 namespace facebook::react {
 
 /*
- * Describes a request to handle a key input.
+ * Describes an individual key event.
  */
-struct HandledKey {
+struct BaseKeyEvent {
   /**
-   * The key for the event aligned to https://www.w3.org/TR/uievents-key/.
+   * The code for the event aligned to https://www.w3.org/TR/uievents-code/.
    */
-  std::string key{};
-
-  /*
-   * A flag indicating if the alt key is pressed.
-   */
-  std::optional<bool> altKey{};
-
-  /*
-   * A flag indicating if the control key is pressed.
-   */
-  std::optional<bool> ctrlKey{};
-
-  /*
-   * A flag indicating if the shift key is pressed.
-   */
-  std::optional<bool> shiftKey{};
-
-  /*
-   * A flag indicating if the meta key is pressed.
-   */
-  std::optional<bool> metaKey{};
-};
-
-inline static bool operator==(const HandledKey& lhs, const HandledKey& rhs) {
-  return lhs.key == rhs.key && lhs.altKey == rhs.altKey &&
-      lhs.ctrlKey == rhs.ctrlKey && lhs.shiftKey == rhs.shiftKey &&
-      lhs.metaKey == rhs.metaKey;
-}
-
-/**
- * Key event emitted by handled key events.
- */
-struct KeyEvent {
-  /**
-   * The key for the event aligned to https://www.w3.org/TR/uievents-key/.
-   */
-  std::string key{};
+  std::string code{};
 
   /*
    * A flag indicating if the alt key is pressed.
@@ -78,58 +44,76 @@ struct KeyEvent {
    * A flag indicating if the meta key is pressed.
    */
   bool metaKey{false};
-
-  /*
-   * A flag indicating if the caps lock key is pressed.
-   */
-  bool capsLockKey{false};
-
-  /*
-   * A flag indicating if the key on the numeric pad is pressed.
-   */
-  bool numericPadKey{false};
-
-  /*
-   * A flag indicating if the help key is pressed.
-   */
-  bool helpKey{false};
-
-  /*
-   * A flag indicating if a function key is pressed.
-   */
-  bool functionKey{false};
 };
 
-inline static bool operator==(const KeyEvent& lhs, const HandledKey& rhs) {
-  return lhs.key == rhs.key &&
-      (!rhs.altKey.has_value() || lhs.altKey == *rhs.altKey) &&
-      (!rhs.ctrlKey.has_value() || lhs.ctrlKey == *rhs.ctrlKey) &&
-      (!rhs.shiftKey.has_value() || lhs.shiftKey == *rhs.shiftKey) &&
-      (!rhs.metaKey.has_value() || lhs.metaKey == *rhs.metaKey);
+inline static bool operator==(const BaseKeyEvent &lhs, const BaseKeyEvent &rhs) {
+  return lhs.code == rhs.code && lhs.altKey == rhs.altKey && lhs.ctrlKey == rhs.ctrlKey &&
+      lhs.shiftKey == rhs.shiftKey && lhs.metaKey == rhs.metaKey;
 }
 
-inline void fromRawValue(
-    const PropsParserContext& context,
-    const RawValue& value,
-    HandledKey& result) {
-  if (value.hasType<std::unordered_map<std::string, RawValue>>()) {
-    auto map = static_cast<std::unordered_map<std::string, RawValue>>(value);
-    for (const auto& pair : map) {
-      if (pair.first == "key") {
-        result.key = static_cast<std::string>(pair.second);
-      } else if (pair.first == "altKey") {
-        result.altKey = static_cast<bool>(pair.second);
-      } else if (pair.first == "ctrlKey") {
-        result.ctrlKey = static_cast<bool>(pair.second);
-      } else if (pair.first == "shiftKey") {
-        result.shiftKey = static_cast<bool>(pair.second);
-      } else if (pair.first == "metaKey") {
-        result.metaKey = static_cast<bool>(pair.second);
-      }
-    }
-  } else if (value.hasType<std::string>()) {
-    result.key = (std::string)value;
+/**
+ * Event phase indicator matching the EventPhase in React.
+ * EventPhase includes None, Capturing, AtTarget, Bubbling.
+ */
+enum class HandledEventPhase { Capturing = 1, Bubbling = 3 };
+
+inline void fromRawValue(const PropsParserContext &context, const RawValue &value, HandledEventPhase &result) {
+  if (value.hasType<int>()) {
+    result = static_cast<HandledEventPhase>((int)value);
+    return;
   }
+
+  LOG(ERROR) << "Unsupported HandledKeyEvent::EventPhase type";
 }
+
+/**
+ * Describes a handled key event declaration.
+ */
+struct HandledKeyEvent : BaseKeyEvent {
+  /**
+   * The phase at which the event should be marked as handled.
+   */
+  HandledEventPhase handledEventPhase{HandledEventPhase::Bubbling};
+};
+
+inline void fromRawValue(const PropsParserContext &context, const RawValue &value, HandledKeyEvent &result) {
+  if (value.hasType<std::unordered_map<std::string, RawValue>>()) {
+    auto map = (std::unordered_map<std::string, RawValue>)value;
+
+    auto attrIterator = map.find("handledEventPhase");
+    if (attrIterator != map.end())
+      fromRawValue(context, attrIterator->second, result.handledEventPhase);
+    attrIterator = map.find("altKey");
+    if (attrIterator != map.end())
+      fromRawValue(context, attrIterator->second, result.altKey);
+    attrIterator = map.find("ctrlKey");
+    if (attrIterator != map.end())
+      fromRawValue(context, attrIterator->second, result.ctrlKey);
+    attrIterator = map.find("metaKey");
+    if (attrIterator != map.end())
+      fromRawValue(context, attrIterator->second, result.metaKey);
+    attrIterator = map.find("shiftKey");
+    if (attrIterator != map.end())
+      fromRawValue(context, attrIterator->second, result.shiftKey);
+    attrIterator = map.find("code");
+    if (attrIterator != map.end())
+      fromRawValue(context, attrIterator->second, result.code);
+    return;
+  }
+
+  LOG(ERROR) << "Unsupported HandledKeyEvent type";
+}
+
+inline static bool operator==(const HandledKeyEvent &lhs, const HandledKeyEvent &rhs) {
+  return lhs.handledEventPhase == rhs.handledEventPhase &&
+      static_cast<BaseKeyEvent>(lhs) == static_cast<BaseKeyEvent>(rhs);
+}
+
+struct KeyEvent : BaseKeyEvent {
+  /**
+   * The key for the event aligned to https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values.
+   */
+  std::string key{};
+};
 
 } // namespace facebook::react
