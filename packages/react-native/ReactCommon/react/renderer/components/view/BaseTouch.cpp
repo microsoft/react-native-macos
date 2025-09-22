c0a348d7540f1d5e05eeb21cb0fc8bcc777e4998
Saad Najmi <sanajmi@microsoft.com>
2025-09-22 11:52:02 -0700

    Remove diffs in BaseTouch.cpp

--- packages/react-native/ReactCommon/react/renderer/components/view/BaseTouch.cpp
+++ packages/react-native/ReactCommon/react/renderer/components/view/BaseTouch.cpp
@@ -23,13 +23,6 @@ void setTouchPayloadOnObject(
   object.setProperty(runtime, "target", touch.target);
   object.setProperty(runtime, "timestamp", touch.timestamp * 1000);
   object.setProperty(runtime, "force", touch.force);
-#if TARGET_OS_OSX // [macOS
-  object.setProperty(runtime, "button", touch.button);
-  object.setProperty(runtime, "altKey", touch.altKey);
-  object.setProperty(runtime, "ctrlKey", touch.ctrlKey);
-  object.setProperty(runtime, "shiftKey", touch.shiftKey);
-  object.setProperty(runtime, "metaKey", touch.metaKey);
-#endif // macOS]
 }
 
 #if RN_DEBUG_STRING_CONVERTIBLE
@@ -49,13 +42,6 @@ std::vector<DebugStringConvertibleObject> getDebugProps(
       {"target", getDebugDescription(touch.target, options)},
       {"force", getDebugDescription(touch.force, options)},
       {"timestamp", getDebugDescription(touch.timestamp, options)},
-#if TARGET_OS_SX // [macOS
-	  {"button", getDebugDescription(touch.button, options)},
-	  {"altKey", getDebugDescription(touch.altKey, options)},
-	  {"ctrlKey", getDebugDescription(touch.ctrlKey, options)},
-	  {"shiftKey", getDebugDescription(touch.shiftKey, options)},
-	  {"metaKey", getDebugDescription(touch.metaKey, options)},
-#endif // macOS]
   };
 }
 
