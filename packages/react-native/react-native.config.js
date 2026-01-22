3e7828a2fba6bd9a700dd6d583a31bc27d2458ba
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2026-01-22 19:26:54 +0100

    weird format but ok

--- packages/react-native/react-native.config.js
+++ packages/react-native/react-native.config.js
@@ -71,10 +71,9 @@ try {
 // [macOS
 let apple;
 try {
-  const iosPath = require.resolve(
-    '@react-native-community/cli-platform-ios',
-    {paths: [process.cwd()]},
-  );
+  const iosPath = require.resolve('@react-native-community/cli-platform-ios', {
+    paths: [process.cwd()],
+  });
   // $FlowFixMe[untyped-import]
   apple = findCommunityPlatformPackage(
     '@react-native-community/cli-platform-apple',
